b1cadf5788dd90cca9911547eeda0a1aa54054ce
bjartur20 <bjartur20@gmail.com>
2020-03-04 12:22:40 +0000

    divisible by five not three test red

--- test_sample.py
+++ test_sample.py
@@ -2,3 +2,7 @@ import sample
 
 def test_not_divisible_by_three_or_five():
     assert sample.fizz_buzz(2) == 2
+
+def test_divisible_by_five_not_three():
+    assert sample.fizz_buzz(5) == 'Fizz'
+
